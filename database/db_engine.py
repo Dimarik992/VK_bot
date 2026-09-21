3f443df5e0f2f71394637f6f725f577bccfe58d6
Anton <4yyort@gmail.com>
2026-09-21 20:50:01 +0300

    refactoring db modules

---
+++ database/db_engine.py
@@ -0,0 +1,49 @@
+from functools import wraps
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from db_model import Base
+from config import DATABASE_URL
+
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
+
+
+def init_db():
+    """
+    Функция инициализации БД
+    """
+    Base.metadata.create_all(engine)
+
+
+def with_session(func):
+    """
+    Декоратор - открывает сессию, коммитит при успехе, откатывает при ошибке
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        session = SessionLocal()
+        try:
+            result = func(session, *args, **kwargs)
+            session.commit()
+            return result
+        except Exception as e:
+            session.rollback()
+            raise e
+        finally:
+            session.close()
+    return wrapper
+
+
+def with_read_session(func):
+    """
+    Декоратор для чтения - - открывает сессию, дальше закрывает сессию, без коммита.
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        session = SessionLocal()
+        try:
+            return func(session, *args, **kwargs)
+        finally:
+            session.close()
+    return wrapper
